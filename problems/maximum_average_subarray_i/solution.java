19f7eb44d545fdb8303090c5d473155ad9e48d90
JAYAPRASANTH VS <167199007+JayaprasanthVS@users.noreply.github.com>
2025-02-27 10:22:58 +0000

    Sync LeetCode submission - Maximum Average Subarray I (java)

---
+++ problems/maximum_average_subarray_i/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum=0;
+        for(int i=0;i<k;i++)
+          sum+=nums[i];
+        int m=sum;
+        for(int i=k;i<nums.length;i++){
+            sum+=nums[i]-nums[i-k];
+            m=Math.max(m,sum);
+
+        }
+        return (double)m/k;    
+    }
+}
