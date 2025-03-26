27f8320f457b712c64a0386404e09d8b3f9a9354
JAYAPRASANTH VS <167199007+JayaprasanthVS@users.noreply.github.com>
2025-03-26 08:47:17 +0000

    Sync LeetCode submission - Contains Duplicate (java)

--- problems/contains_duplicate/solution.java
+++ problems/contains_duplicate/solution.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        for (int i = 1; i < n; i++) {
-            if (nums[i] == nums[i - 1])
-                return true;
+        Arrays.sort(nums);boolean b=false;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]) {
+                b=true;
+                break;
+            }
         }
-        return false;
+        return b;
     }
 }
