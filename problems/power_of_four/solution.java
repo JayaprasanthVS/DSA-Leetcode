dd5db99bc0c850e15a44309157b36c4165808c7e
JAYAPRASANTH VS <167199007+JayaprasanthVS@users.noreply.github.com>
2025-03-28 08:06:11 +0000

    Sync LeetCode submission - Power of Four (java)

---
+++ problems/power_of_four/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        // Base cases
+        if (n == 1) return true;  
+        if (n == 0 || n % 4 != 0) return false;  
+        
+        // Recursive step
+        return isPowerOfFour(n / 4);
+    }
+}
