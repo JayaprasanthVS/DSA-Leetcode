db082f4e270f66588d994108441a659cd3f2c0f7
JAYAPRASANTH VS <167199007+JayaprasanthVS@users.noreply.github.com>
2025-06-20 05:58:38 +0000

    Sync LeetCode submission - Climbing Stairs (golang)

---
+++ problems/climbing_stairs/solution.go
@@ -0,0 +1,10 @@
+func climbStairs(n int) int {
+    if (n == 0 || n == 1){
+        return 1;
+    }
+   a, b := 1, 1
+    for i := 2; i <= n; i++ {
+        a, b = b, a+b
+    }
+    return b
+    }
