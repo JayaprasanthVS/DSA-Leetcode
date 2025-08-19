8633820b201f5cfc26e4fe7ca6a3b3f3baf79755
JAYAPRASANTH VS <167199007+JayaprasanthVS@users.noreply.github.com>
2025-08-19 04:32:45 +0000

    Sync LeetCode submission - Repeated DNA Sequences (java)

---
+++ problems/repeated_dna_sequences/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+                Set<String> seen = new HashSet<>();
+        Set<String> repeated = new HashSet<>();
+        
+        // Only check if string is longer than 10
+        for (int i = 0; i + 10 <= s.length(); i++) {
+            String substring = s.substring(i, i + 10);
+            if (!seen.add(substring)) {
+                repeated.add(substring);
+            }
+        }
+        
+        return new ArrayList<>(repeated);
+    }
+}
