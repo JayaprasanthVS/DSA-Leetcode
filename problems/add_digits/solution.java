c97f746e47940bbe67401b4b8b34daa1bd366035
JAYAPRASANTH VS <167199007+JayaprasanthVS@users.noreply.github.com>
2025-08-25 07:39:00 +0000

    Sync LeetCode submission - Add Digits (java)

--- problems/add_digits/solution.java
+++ problems/add_digits/solution.java
@@ -1,11 +1,5 @@
 class Solution {
     public int addDigits(int num) {
-        if(num<10) return num;int s=0;
-        while(num>0){
-            int t=num%10;
-            s+=t;
-            num/=10;
-        }
-        return addDigits(s);
+        return 1 + (num - 1) % 9;
     }
 }
