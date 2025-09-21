6baede44bddec332c5a84845a1d23a8d39755bfc
JAYAPRASANTH VS <167199007+JayaprasanthVS@users.noreply.github.com>
2025-09-21 02:42:19 +0000

    Sync LeetCode submission - Bitwise OR of Even Numbers in an Array (java)

---
+++ problems/bitwise_or_of_even_numbers_in_an_array/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int evenNumberBitwiseORs(int[] nums) {
+        int ans = 0;
+        for ( int i = 0; i<nums.length; i++){
+           if(nums[i]%2 != 1){
+               ans|=nums[i];}
+        }
+        return ans;
+    }
+}
