653e8efdc81b36fc40ce2f68b6df5f0d0ebf3d0f
JAYAPRASANTH VS <167199007+JayaprasanthVS@users.noreply.github.com>
2025-12-17 10:40:41 +0000

    Sync LeetCode submission - N-th Tribonacci Number (java)

--- problems/n-th_tribonacci_number/solution.java
+++ problems/n-th_tribonacci_number/solution.java
@@ -1,11 +1,21 @@
 class Solution {
+    public int solve(int n, int dp[]){
+        if( dp[n] != -1 ) return dp[n];
+        //if( n == 0 || n == 1 || n == 2) return dp[n];
+        else{
+            dp[n] = solve(n-1,dp)+solve(n-2,dp)+solve(n-3,dp);
+        }
+        return dp[n];
+    }
     public int tribonacci(int n) {
-       int f[]=new int [42];
-       f[0]=0;f[1]=1;f[2]=1;
-       for(int i=0;i<n;i++){
-        f[i+3]=f[i]+f[i+1]+f[i+2];
-
-       }
-       return f[n];
+        if (n==0) return n;
+        if( n == 1 || n == 2) return 1;
+        int dp[] = new int[n+2];
+        
+        Arrays.fill(dp,-1);
+        dp[0] = 0;dp[1] = 1; dp[2] = 1;
+        
+        //if( n == 1 || n == 2) return 1;
+        return solve(n,dp);
     }
 }
