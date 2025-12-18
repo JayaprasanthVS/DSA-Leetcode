c9c7e60564a461b1780237ddbed54177f2d5a84e
JAYAPRASANTH VS <167199007+JayaprasanthVS@users.noreply.github.com>
2025-12-18 09:00:15 +0000

    Sync LeetCode submission - Edit Distance (java)

--- problems/edit_distance/solution.java
+++ problems/edit_distance/solution.java
@@ -1,23 +1,26 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-      int n =  word1.length(),m = word2.length();
-      int dp[] = new int[m+1];
-      for( int i = 0; i<m+1; i++ ) {
-        dp[i] = i;
-      }
-      for ( int i = 1; i< n+1; i++ ) {
-        int prevDiag = i-1;
-        dp[0] = i;
-        for( int j = 1; j< m+1; j++ ) {
-            int prevTop = dp[j];
-            if( word1.charAt(i-1) == word2.charAt(j-1)) {
-                dp[j] = prevDiag;
-            } else {
-                dp[j] = Math.min ( dp[j],Math.min(dp[j-1], prevDiag))+1;
-            }
-            prevDiag = prevTop;
-        }
-      }
-      return dp[m];
+        int n=word1.length();
+        int m=word2.length();
+        
+        int dp[][]=new int[n][m];
+        for(int r[]:dp) Arrays.fill(r,-1);
+        return rec(word1,word2,n-1,m-1,dp);
     }
+    public int rec(String word1, String word2,int i,int j, int[][] dp){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(word1.charAt(i)==word2.charAt(j)) return rec(word1,word2,i-1,j-1,dp);
+
+        int delet=rec(word1,word2,i-1,j,dp);
+        int insert=rec(word1,word2,i,j-1,dp);
+        int replace=rec(word1,word2,i-1,j-1,dp);
+
+        return dp[i][j]=1+Math.min(delet,Math.min(insert,replace));
+        
+        
+    } 
 }
