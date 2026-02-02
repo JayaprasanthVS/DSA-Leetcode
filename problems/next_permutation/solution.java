065bf25f595b345071ca11d4cfa235e664b089a6
JAYAPRASANTH VS <167199007+JayaprasanthVS@users.noreply.github.com>
2026-02-02 05:03:49 +0000

    Sync LeetCode submission - Next Permutation (java)

---
+++ problems/next_permutation/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        
+        int n = nums.length;
+        int i = n - 2;
+
+        
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        // 3️⃣ Reverse suffix (always)
+        reverse(nums, i + 1, n - 1);
+    }
+
+    void swap(int[] a, int i, int j) {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    void reverse(int[] a, int l, int r) {
+        while (l < r) {
+            swap(a, l++, r--);
+        }
+    }
+}
