462a99488432d656bb85df8d43aeb235b663dc17
JAYAPRASANTH VS <167199007+JayaprasanthVS@users.noreply.github.com>
2026-02-04 04:58:10 +0000

    Sync LeetCode submission - Merge Strings Alternately (java)

--- problems/merge_strings_alternately/solution.java
+++ problems/merge_strings_alternately/solution.java
@@ -1,17 +1,28 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-      StringBuilder result = new StringBuilder();
+        int n = word1.length();
+        int m = word2.length();
+
+        char[] output = new char[n+m];
+
         int i = 0;
-        while (i < word1.length() || i < word2.length()) {
-            if (i < word1.length()) {
-                result.append(word1.charAt(i));
-            }
-            if (i < word2.length()) {
-                result.append(word2.charAt(i));
-            }
-            i++;
+        int j = 0;
+
+        int x = 0;
+
+        while (i<n && j<m) {
+            output[x++] = word1.charAt(i++); 
+            output[x++] = word2.charAt(j++); 
         }
-        
-        return result.toString();
+
+        while (i<n) {
+            output[x++] = word1.charAt(i++); 
+        }
+
+        while (j<m) {
+            output[x++] = word2.charAt(j++); 
+        }
+
+        return new String(output);
     }
 }
