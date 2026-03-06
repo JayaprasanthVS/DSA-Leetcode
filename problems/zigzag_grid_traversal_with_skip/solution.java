815cfebb2dc2f6d8693b0f4337d86a278c1b2c33
JAYAPRASANTH VS <167199007+JayaprasanthVS@users.noreply.github.com>
2026-03-06 08:43:07 +0000

    Sync LeetCode submission - Zigzag Grid Traversal With Skip (java)

---
+++ problems/zigzag_grid_traversal_with_skip/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> zigzagTraversal(int[][] grid) {
+        List<Integer> ans = new ArrayList<Integer>();
+        for ( int i = 0 ;i <grid.length;  i++ ){
+            if (i%2 == 0){
+            for ( int j  =0 ; j<grid[0].length;  j++){
+                if ((i+j)%2 == 0) ans.add(grid[i][j]);
+            }}
+            else {
+                for ( int j = grid[i].length-1; j>-1;j--){
+                    if  ((i+j)%2 == 0 ) ans.add(grid[i][j]);
+                }
+            }
+
+        }
+        return ans;
+    }
+    
+}
